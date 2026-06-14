476e5a1fdec618528c6d8c53c4cafa120088d3a1
codingwithriya <itisghoshriya@gmail.com>
2026-06-15 00:36:14 +0530

    Day 2 Extended

--- src/main/java/com/codewithriya/MyFirstProject/StudentController/StudentController.java
+++ src/main/java/com/codewithriya/MyFirstProject/StudentController/StudentController.java
@@ -3,10 +3,7 @@ package com.codewithriya.MyFirstProject.StudentController;
 import com.codewithriya.MyFirstProject.Models.Student;
 import com.codewithriya.MyFirstProject.StudentRepo.StudRepo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -27,4 +24,20 @@ public class StudentController {
         Student s1 = studrepo.findById(id).get();
         return s1;
     }
+
+    @PostMapping("/createStudent")
+    public List<Student> createStudent (@RequestBody Student s1){
+        studrepo.save(s1);
+        List<Student> Stdl = studrepo.findAll();
+        return Stdl;
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public List<Student> deleteStudent (@PathVariable int id){
+        Student s1 = studrepo.findById(id).get();
+        studrepo.delete(s1);
+        List<Student> Stdl = studrepo.findAll();
+        return Stdl;
+    }
+
 }
